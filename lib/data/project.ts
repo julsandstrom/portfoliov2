e13ed4f87a5ba3a844478b318d3650a3673d9ebe
juliansandstrom <juliiansandstrom@gmail.com>
2025-11-03 05:16:59 +0100

    font and icons added

---
+++ lib/data/project.ts
@@ -0,0 +1,10 @@
+export const projects = [
+  {
+    title: 'Puppeteer Me',
+    image: '/images/PuppeteerMobile.png',
+  },
+  {
+    title: 'Mekoja',
+    image: '/images/MekojaMobile.png',
+  },
+];
