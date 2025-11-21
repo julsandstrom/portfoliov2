5a62f95d690732038a5cab5db38e1bca71d0eb6e
unknown <juliiansandstrom@gmail.com>
2025-11-21 11:17:39 +0100

    correct cartlag link

--- lib/data/project.ts
+++ lib/data/project.ts
@@ -1,43 +1,43 @@
 export const projects = [
   {
-    title: 'Cartlag',
-    image: '/images/CartlagMobile.png',
-    overlayTitle: 'A body measurement app',
+    title: "Cartlag",
+    image: "/images/CartlagMobile.png",
+    overlayTitle: "A body measurement app",
     overlayBody:
-      'Generate a personal size card you can download, update, and reuse across brands for accurate fit.',
-    url: 'https://puppeteerme.com/',
+      "Generate a personal size card you can download, update, and reuse across brands for accurate fit.",
+    url: "https://cartlag.com/",
   },
   {
-    title: 'Driftus',
-    image: '/images/DriftusMobile.png',
-    overlayTitle: 'A chat app',
+    title: "Driftus",
+    image: "/images/DriftusMobile.png",
+    overlayTitle: "A chat app",
     overlayBody:
-      'showing only the latest message, with AI stepping in when words run out.',
-    url: 'https://driftus.netlify.app/login',
+      "showing only the latest message, with AI stepping in when words run out.",
+    url: "https://driftus.netlify.app/login",
   },
   {
-    title: 'Puppeteer Me',
-    image: '/images/PuppeteerMobile.png',
-    overlayTitle: 'A quiz about someone you know',
+    title: "Puppeteer Me",
+    image: "/images/PuppeteerMobile.png",
+    overlayTitle: "A quiz about someone you know",
     overlayBody:
-      'in a minimalist yet carnival-inspired quiz on perception and identity.',
-    url: 'https://puppeteerme.com/',
+      "in a minimalist yet carnival-inspired quiz on perception and identity.",
+    url: "https://puppeteerme.com/",
   },
   {
-    title: 'Mekoja',
-    image: '/images/MekojaMobile.png',
-    overlayTitle: 'An interactive platform',
+    title: "Mekoja",
+    image: "/images/MekojaMobile.png",
+    overlayTitle: "An interactive platform",
     overlayBody:
-      'to rank your values and share or receive guidance from like-minded people.',
-    url: 'https://julsandstrom.github.io/mekoja/',
+      "to rank your values and share or receive guidance from like-minded people.",
+    url: "https://julsandstrom.github.io/mekoja/",
   },
 
   {
-    title: 'Return Cascade',
-    image: '/images/ReturnCascadeMobile.png',
-    overlayTitle: 'A juggling-inspired FPS in the snow',
+    title: "Return Cascade",
+    image: "/images/ReturnCascadeMobile.png",
+    overlayTitle: "A juggling-inspired FPS in the snow",
     overlayBody:
-      'push enemies, solve puzzles, and balance your overheating weapon like keeping three balls in the air.',
-    url: 'https://youtu.be/Vfq_1EhHR1I',
+      "push enemies, solve puzzles, and balance your overheating weapon like keeping three balls in the air.",
+    url: "https://youtu.be/Vfq_1EhHR1I",
   },
 ];
